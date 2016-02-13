cc4aa5a7e7d33613213756c6f09b8d9211870620
K-2052 <k@2052.me>
2016-02-13 11:20:49 -0800

    update webpack config to use development instead of dev as NODE_ENV value

--- webpack.client.config.js
+++ webpack.client.config.js
@@ -4,21 +4,21 @@ var path = require('path');
 var args = require('minimist')(process.argv.slice(2));
 
 // List of allowed environments
-var allowedEnvs = ['dev', 'production', 'test'];
+var allowedEnvs = ['development', 'production', 'test'];
 
 // Set the correct environment
 var env;
 if(process.env.NODE_ENV) {
   env = process.env.NODE_ENV;
 } else {
-  env = 'dev';
+  env = 'development';
 }
 process.env.REACT_WEBPACK_ENV = env;
 
 // Get available configurations
 var configs = {
   base: require(path.join(__dirname, 'cfg/webpack.base')),
-  dev: require(path.join(__dirname,  'cfg/client/webpack.dev')),
+  development: require(path.join(__dirname,  'cfg/client/webpack.dev')),
   production: require(path.join(__dirname, 'cfg/client/webpack.dist')),
   test: require(path.join(__dirname, 'cfg/client/webpack.test'))
 };
@@ -30,7 +30,7 @@ var configs = {
  */
 function getValidEnv(env) {
   var isValid = env && env.length > 0 && allowedEnvs.indexOf(env) !== -1;
-  return isValid ? env : 'dev';
+  return isValid ? env : 'development';
 }
 
 /**
