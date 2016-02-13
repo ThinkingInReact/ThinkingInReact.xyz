93ea18f6b546cebbd39913e4f31e3b87789c3b29
K-2052 <k@2052.me>
2016-02-13 12:01:27 -0800

    move serialize function into separate file

---
+++ src/common/lib/serialize.js
@@ -0,0 +1,7 @@
+const serialize = function (data) {
+  return Object.keys(data).map(function (keyName) {
+    return encodeURIComponent(keyName) + '=' + encodeURIComponent(data[keyName])
+  }).join('&');
+};
+
+export default serialize;
