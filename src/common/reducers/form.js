430aa9219b4cadc2c85a8e9119e7f0768b517d08
K-2052 <k@2052.me>
2016-02-13 11:54:06 -0800

    clean up form reducer

--- src/common/reducers/form.js
+++ src/common/reducers/form.js
@@ -8,28 +8,6 @@ function form(state = {}, action) {
     buy = {};
   }
 
- /**
-  *
-  * TODO: Make this work
-  if(action.type == 'redux-form/CHANGE') {
-    if(action.field == 'email') {
-      let githubUser = {value: action.value.split('@')[0] };
-
-      if(buy.githubUser) {
-        if(buy.githubUser.visited) {
-          githubUser = { };
-        }
-      }
-
-      return {
-        ...state,
-        ...nextState,
-        buy: Object.assign({}, buy, { githubUser })
-      };
-    }
-  }
-  */
-
   return nextState;
 }
 
