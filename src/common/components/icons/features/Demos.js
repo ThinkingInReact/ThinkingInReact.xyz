ae66f63cd23881d788840724ccfc85e2cd2306f1
K-2052 <k@2052.me>
2016-02-14 12:11:08 -0800

    add icons

---
+++ src/common/components/icons/features/Demos.js
@@ -0,0 +1,18 @@
+/**
+ * Icon based on ion-ios-monitor-outline
+ */
+
+import React, { Component } from 'react';
+
+class Demos extends Component {
+  render() {
+    return (
+      <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+      	 width="512px" height="512px" viewBox="0 0 512 512">
+        <path d="M496,384V96H16v288h175v16h-64v16h257v-16h-64v-16H496z M32,112h448v256H32V112z"/>
+      </svg>
+    )
+  }
+}
+
+export default Demos;
